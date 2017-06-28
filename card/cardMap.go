b4fc2556b4790e2b4f18cf913e9e034e963e1d1a
王虎 <wanghu@maxpanda.cn>
2017-06-28 12:48:58 +0800

    添加手牌map结构

--- card/cardMap.go
+++ card/cardMap.go
@@ -1,23 +1,88 @@
 package card
 
-import "sort"
+import (
+	"sort"
+	"sync"
+)
 
-// CardMap 牌面=>数量
-type CardMap map[int]int
-
-// NewCardMap 初始化一个TileMap
-func NewCardMap() CardMap {
-	return make(CardMap)
+// CMap 牌面=>数量
+type CMap struct {
+	Mux   *sync.RWMutex
+	tiles map[int]int
 }
 
-// ToSlice 将TileMap的格式转成[]int
-func (cm CardMap) ToSlice() []int {
+// NewCMap 初始化一个TileMap
+func NewCMap() *CMap {
+	return &CMap{
+		Mux:   &sync.RWMutex{},
+		tiles: make(map[int]int),
+	}
+}
+
+// SetTiles 初始化手牌
+func (cm *CMap) SetTiles(tiles []int) {
+	cm.Mux.Lock()
+	defer cm.Mux.Unlock()
+	for _, tile := range tiles {
+		cm.tiles[tile]++
+	}
+}
+
+// GetTileMap 读取所有牌的列表
+// 这里不会主动加锁，在外面用的话，如果用于range，需要手动加锁
+func (cm *CMap) GetTileMap() map[int]int {
+	return cm.tiles
+}
+
+// AddTile 添加手牌
+func (cm *CMap) AddTile(tile, cnt int) {
+	cm.Mux.Lock()
+	defer cm.Mux.Unlock()
+	cm.tiles[tile] += cnt
+}
+
+// DelTile 删除手牌
+func (cm *CMap) DelTile(tile, cnt int) bool {
+	cm.Mux.Lock()
+	defer cm.Mux.Unlock()
+	if cm.tiles[tile] > cnt {
+		cm.tiles[tile] -= cnt
+	} else if cm.tiles[tile] == cnt {
+		delete(cm.tiles, tile)
+	} else {
+		return false
+	}
+	return true
+}
+
+// ToSlice 转成slice
+func (cm *CMap) ToSlice() []int {
+	cm.Mux.RLock()
+	defer cm.Mux.RUnlock()
 	tiles := []int{}
-	for tile, cnt := range cm {
+	for tile, cnt := range cm.tiles {
 		for i := 0; i < cnt; i++ {
 			tiles = append(tiles, tile)
 		}
 	}
 	sort.Ints(tiles)
+	return tiles
+}
+
+// ToSortedSlice 转成slice并排序
+func (cm *CMap) ToSortedSlice() []int {
+	tiles := cm.ToSlice()
+	sort.Ints(tiles)
+	return tiles
+}
+
+// GetUnique 获取独立的牌
+func (cm *CMap) GetUnique() []int {
+	cm.Mux.RLock()
+	defer cm.Mux.RUnlock()
+	tiles := []int{}
+	for tile := range cm.tiles {
+		tiles = append(tiles, tile)
+	}
 	return tiles
 }
---
+++ card/cardMap_test.go
@@ -0,0 +1,30 @@
+package card
+
+import (
+	"testing"
+)
+
+func TestCardMap(t *testing.T) {
+	initTiles := []int{3, 2, 1, 7, 9, 8, 7, 3, 1}
+	cm := NewCMap()
+	cm.SetTiles(initTiles)
+
+	t.Logf("cm.tiles:%#v", cm.GetTileMap())
+	t.Logf("cm.GetUnique:%v", cm.GetUnique())
+	t.Logf("cm.ToSlice:%v", cm.ToSlice())
+	t.Logf("cm.ToSortedSlice:%v", cm.ToSortedSlice())
+	cm.DelTile(1, 1)
+	t.Logf("cm.ToSortedSlice:%v", cm.ToSortedSlice())
+	cm.DelTile(1, 1)
+	t.Logf("cm.ToSortedSlice:%v", cm.ToSortedSlice())
+	cm.DelTile(3, 2)
+	t.Logf("cm.ToSortedSlice:%v", cm.ToSortedSlice())
+	flag := cm.DelTile(3, 1)
+	t.Logf("cm.DelTile:%v", flag)
+	cm.AddTile(3, 1)
+	t.Logf("cm.ToSortedSlice:%v", cm.ToSortedSlice())
+	cm.AddTile(3, 1)
+	t.Logf("cm.ToSortedSlice:%v", cm.ToSortedSlice())
+	cm.AddTile(1, 2)
+	t.Logf("cm.ToSortedSlice:%v", cm.ToSortedSlice())
+}
